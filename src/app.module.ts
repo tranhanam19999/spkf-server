afe1921716a8da4bafcc73c721b4090b81de6302
vudung2000 <dangvudung18110263@gmail.com>
2021-05-03 17:04:17 +0700

    add package nest.config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PostsModule, MongooseModule.forRoot('mongodb://localhost:27017/spkf-db')],
+  imports: [ConfigModule.forRoot(),PostsModule, MongooseModule.forRoot(process.env.MONGO_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
